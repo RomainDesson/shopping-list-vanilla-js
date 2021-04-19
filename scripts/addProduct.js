6316a0dd71f758440d3dc6f5d31227358ec3a957
Romain <romain.desson.02320@gmail.com>
2021-04-19 15:52:44 +0100

    add the basic verifs to "add product"

--- scripts/addProduct.js
+++ scripts/addProduct.js
@@ -2,8 +2,22 @@ const addButton = document.querySelector(".add-button")
 const productInput = document.querySelector(".product-input")
 const ul = document.querySelector(".shopping-list")
 
+// Main function to add the input value as a child of the list
+const addProductToList = () => {
+    if (productInput.value !== '' && productInput.value[0] !== ' ') {
+        const li = document.createElement("li")
+        ul.appendChild(li)
+        li.appendChild(document.createTextNode(productInput.value))
+        productInput.value = ''
+    }
+}
+// function to add the input value on the add button
 addButton.addEventListener('click', () => {
-    const li = document.createElement("li")
-    ul.appendChild(li)
-    li.appendChild(document.createTextNode(productInput.value))
+    addProductToList()
+})
+// function to add the input value on press enter
+productInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addProductToList()
+    }
 })
